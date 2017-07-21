c6491addf3be95d17de9a0a44a82bc95a5472305
Dylan Orzel <dylan.orzel@gmail.com>
2017-07-21 16:14:58 -0600

    add basic tests

--- tests/test_greengrass_base_block.py
+++ tests/test_greengrass_base_block.py
@@ -1,19 +1,85 @@
-from nio.block.terminals import DEFAULT_TERMINAL
-from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..example_block import Example
+from ..greengrass_mqtt_base_block import GreenGrassMQTTBase
+from ..greengrass_mqtt_subscribe_block import GreenGrassMQTTSubscribe
+from ..greengrass_mqtt_publish_block import GreenGrassMQTTPublish
+from ..greengrass_shadow_base_block import GreenGrassMQTTShadowBase
+from ..greengrass_shadow_update_block import GreenGrassShadowUpdate
+from ..greengrass_shadow_delete_block import GreenGrassShadowDelete
+from ..greengrass_shadow_delta_listener_block import GreenGrassShadowDeltaListener
 
 
-class TestExample(NIOBlockTestCase):
+class TestMQTTBase(NIOBlockTestCase):
 
     def test_process_signals(self):
         """Signals pass through block unmodified."""
-        blk = Example()
+        blk = GreenGrassMQTTBase()
         self.configure_block(blk, {})
         blk.start()
-        blk.process_signals([Signal({"hello": "n.io"})])
         blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "n.io"})
+        self.assert_num_signals_notified(0)
+
+
+class TestMQTTSubscribe(NIOBlockTestCase):
+
+    def test_process_signals(self):
+        """Signals pass through block unmodified."""
+        blk = GreenGrassMQTTSubscribe()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.stop()
+        self.assert_num_signals_notified(0)
+
+
+class TestMQTTPublish(NIOBlockTestCase):
+
+    def test_process_signals(self):
+        """Signals pass through block unmodified."""
+        blk = GreenGrassMQTTPublish()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.stop()
+        self.assert_num_signals_notified(0)
+
+
+class TestMQTTShadowBase(NIOBlockTestCase):
+
+    def test_process_signals(self):
+        """Signals pass through block unmodified."""
+        blk = GreenGrassMQTTShadowBase()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.stop()
+        self.assert_num_signals_notified(0)
+
+
+class TestMQTTShadowUpdate(NIOBlockTestCase):
+
+    def test_process_signals(self):
+        """Signals pass through block unmodified."""
+        blk = GreenGrassShadowUpdate()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.stop()
+        self.assert_num_signals_notified(0)
+
+
+class TestMQTTShadowDelete(NIOBlockTestCase):
+
+    def test_process_signals(self):
+        """Signals pass through block unmodified."""
+        blk = GreenGrassShadowDelete()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.stop()
+        self.assert_num_signals_notified(0)
+
+
+class TestMQTTShadowDeltaListener(NIOBlockTestCase):
+
+    def test_process_signals(self):
+        """Signals pass through block unmodified."""
+        blk = GreenGrassShadowDeltaListener()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.stop()
+        self.assert_num_signals_notified(0)
